f5aac439eb48f865a24d8d93fac447d92d249b25
Ayush Mishra <41970895+geth-14@users.noreply.github.com>
2019-05-30 22:38:45 +0530

    Update Model.py

--- Model.py
+++ Model.py
@@ -9,21 +9,20 @@ from keras.preprocessing import sequence
 from keras.layers import Dense, LSTM, Dropout, Activation
 
 df = pd.read_csv('stock data.csv')
-print(np.max(df.iloc[0:1234,1]), np.min(df.iloc[0:1234,1]))
-train_set = df.iloc[0:1234,1:2].values
-test_set = df.iloc[1234:1254,1:2].values
+print(np.max(df.iloc[0:1013,1]), np.min(df.iloc[0:1013,1]))
+train_set = df.iloc[0:995,1:2].values
+test_set = df.iloc[995:1013,1:2].values
 scaler = MinMaxScaler(feature_range = (0,1))
 train_set_scaled = scaler.fit_transform(train_set)
 
 X_train = []  
 Y_train = [] 
-for i in range(60, 1234):  
+for i in range(60, 995):  
     X_train.append(train_set_scaled[i-60:i, 0])
     Y_train.append(train_set_scaled[i, 0])
     
 X_train = np.array(X_train)
 Y_train = np.array(Y_train)
-
 X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
 
 model = Sequential()
@@ -40,18 +39,16 @@ model.add(LSTM(units=60))
 model.add(Dropout(0.2))
 
 model.add(Dense(units = 1))  
-
 model.compile(optimizer = 'adam', loss = 'mean_squared_error')
+model.fit(X_train, Y_train, epochs = 150, batch_size = 32)
 
-model.fit(X_train, Y_train, epochs = 100, batch_size = 32)
-
-tot = pd.concat((df.iloc[0:1234,:]['open'], df.iloc[1234:1254,:]['open']), axis=0)
-test_ip = tot[len(tot) - len(df.iloc[1234:1254,:]) - 60:].values  
+tot = pd.concat((df.iloc[0:995,:]['open'], df.iloc[995:1013,:]['open']), axis=0)
+test_ip = tot[len(tot) - len(df.iloc[995:1013,:]) - 60:].values  
 
 test_ip = scaler.transform(test_ip.reshape(-1,1))  
 
 X_test = []  
-for i in range(60, 80):  
+for i in range(60, 78):  
     X_test.append(test_ip[i-60:i, 0])
 
 X_test = np.array(X_test)  
@@ -68,7 +65,7 @@ plt.ylabel('Stock Price')
 plt.legend()  
 plt.show()
 
-Actual = np.array(df.iloc[1234:1254,1]) 
+Actual = np.array(df.iloc[995:1013,1]) 
 Predicted = predict  
 print('Mean Absolute Error:', metrics.mean_absolute_error(Actual, Predicted))  
 print('Mean Squared Error:', metrics.mean_squared_error(Actual, Predicted))  
